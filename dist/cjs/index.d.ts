cd90cc956734d5a47401fb8e9c218a57d11fd992
TopTech126 <carlie19782@gmail.com>
2024-07-16 04:40:36 +1000

    init

---
+++ dist/cjs/index.d.ts
@@ -0,0 +1,7 @@
+import Connector from "@luxfi/wc-core";
+import { IWalletConnectOptions, IPushServerOptions } from "@luxfi/wc-types";
+declare class WalletConnect extends Connector {
+    constructor(connectorOpts: IWalletConnectOptions, pushServerOpts?: IPushServerOptions);
+}
+export default WalletConnect;
+//# sourceMappingURL=index.d.ts.map
